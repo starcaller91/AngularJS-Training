32cde2e04bf015436aefb452ca2708ce547defe7
Stefan <s.popov@levi9.com>
2016-10-26 10:57:48 +0200

    +

---
+++ wwwroot/appScripts/wwwroot/libs/rxjs/src/operator/concatMap.js
@@ -0,0 +1,7 @@
+"use strict";
+var mergeMap_1 = require('./mergeMap');
+/* tslint:disable:max-line-length */
+function concatMap(project, resultSelector) {
+    return this.lift(new mergeMap_1.MergeMapOperator(project, resultSelector, 1));
+}
+exports.concatMap = concatMap;
